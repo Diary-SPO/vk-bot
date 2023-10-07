1384d3890da66ef008f83d0999c3f5babae5a58c
scffs <khidey@inbox.ru>
2023-10-07 12:49:10 +0700

    review: не забываем о стиле именования

--- src/dblogic/schedule.ts
+++ src/dblogic/schedule.ts
@@ -3,7 +3,7 @@ import fetcher from '@src/api/fetcher'
 import { SERVER_URL } from '@src/config'
 import { type IGetSchedule } from '@types'
 
-export default async (diaryid: number, date: Date, localCache: boolean, cookie: string) : Promise<unknown> => {
+export default async (diaryId: number, date: Date, localCache: boolean, cookie: string) : Promise<unknown> => {
     const dateString = `${date.getFullYear()}-${('0' + (date.getMonth() + 1)).slice(-2)}-${('0' + (date.getDay())).slice(-2)}`
 
     if (localCache) {
@@ -11,7 +11,7 @@ export default async (diaryid: number, date: Date, localCache: boolean, cookie: 
     }
 
     const res = await fetcher<IGetSchedule[]>({
-        url: `${SERVER_URL}/students/${diaryid}/lessons/${dateString}/${dateString}`,
+        url: `${SERVER_URL}/students/${diaryId}/lessons/${dateString}/${dateString}`,
         method: 'GET',
         cookie: cookie
       })
